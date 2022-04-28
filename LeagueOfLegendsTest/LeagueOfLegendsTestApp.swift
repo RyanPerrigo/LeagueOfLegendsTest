25cbb5b34fa6c02baaa2b4a9789695417bdd06de
Imantium <Ryan.Perrigo@gmail.com>
2022-04-28 18:39:31 -0500

    Initial Commit

---
+++ LeagueOfLegendsTest/LeagueOfLegendsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueOfLegendsTestApp.swift
+//  LeagueOfLegendsTest
+//
+//  Created by Ryan Perrigo on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct LeagueOfLegendsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
