421cd75d407a895ddea3b967765d6d760e28f96e
Imantium <Ryan.Perrigo@gmail.com>
2022-05-03 14:06:03 -0500

    added champions Json first commit

--- LeagueOfLegendsTest/ContentView.swift
+++ LeagueOfLegendsTest/ContentView.swift
@@ -6,11 +6,72 @@
 //
 
 import SwiftUI
+import Combine
+
+final class APIManager {
+    
+    
+    func readLocalJSONFile(forName name: String) -> Data? {
+        do {
+            if let filePath = Bundle.main.path(forResource: name, ofType: "json") {
+                let fileUrl = URL(fileURLWithPath: filePath)
+                let data = try Data(contentsOf: fileUrl)
+                return data
+            }
+        } catch {
+            print("error: \(error)")
+        }
+        return nil
+    }
+    
+    func decodeChampions(_ champions:@escaping (TopLevelChampionsEntity) -> Void) {
+        let decoder = JSONDecoder()
+        guard let data = readLocalJSONFile(forName: "Champions") else {debugPrint( "failed to get data from local Json File") ;return}
+        
+        guard let decodedData: TopLevelChampionsEntity = try? decoder.decode(TopLevelChampionsEntity.self, from: data) else { debugPrint("failed to decode Data from local Json File"); return}
+        champions(decodedData)
+    }
+}
+
+class ContentViewModel: ObservableObject {
+    
+    let apiManager = APIManager()
+    var observers: Set<AnyCancellable> = .init()
+    
+    func getDecodedChampions() -> Future<TopLevelChampionsEntity, Never> {
+        
+        return Future { [weak self] promise in
+            
+            self?.apiManager.decodeChampions { champs in
+                promise(.success(champs))
+            }
+        }
+    }
+    func getStats()  {
+        let allChampsData = getDecodedChampions().map { champs in
+            return champs.data
+        }.map { dict in
+           return dict.values
+        }.map { dataValues in
+            dataValues.map { data in
+                data.stats.filter { dictThing in
+                    dictThing.
+                }
+            }
+        }
+        observers.insert(champions)
+      let data = champions.map { topLevelChampionsEntity in
+            return topLevelChampionsEntity.data
+      }.eraseToAnyPublisher()
+    }
+}
 
 struct ContentView: View {
+    
+   @ObservedObject var vm: ContentViewModel = ContentViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
     }
 }
 
---
+++ LeagueOfLegendsTest/ResponseEntities/ResponseEntities.swift
@@ -0,0 +1,66 @@
+//
+//  ResponseEntities.swift
+//  LeagueOfLegendsTest
+//
+//  Created by Ryan Perrigo on 4/28/22.
+//
+
+import Foundation
+
+struct TopLevelChampionsEntity: Decodable {
+    let type: TypeEnum?
+    let format: String?
+//    let version: Version?
+    let data: [String: Datum]
+}
+
+// MARK: - Datum
+struct Datum: Codable {
+//    let version: Version
+    let id, key, name, title: String
+    let blurb: String
+    let info: Info
+//    let image: Image
+    let tags: [Tag]
+    let partype: String
+    let stats: [String: Double]
+}
+
+// MARK: - Image
+//struct Image: Codable {
+//    let full: String
+//    let sprite: Sprite
+//    let group: TypeEnum
+//    let x, y, w, h: Int
+//}
+
+enum TypeEnum: String, Codable {
+    case champion = "champion"
+}
+
+//enum Sprite: String, Codable {
+//    case champion0PNG = "champion0.png"
+//    case champion1PNG = "champion1.png"
+//    case champion2PNG = "champion2.png"
+//    case champion3PNG = "champion3.png"
+//    case champion4PNG = "champion4.png"
+//    case champion5PNG = "champion5.png"
+//}
+
+// MARK: - Info
+struct Info: Codable {
+    let attack, defense, magic, difficulty: Int
+}
+
+enum Tag: String, Codable {
+    case assassin = "Assassin"
+    case fighter = "Fighter"
+    case mage = "Mage"
+    case marksman = "Marksman"
+    case support = "Support"
+    case tank = "Tank"
+}
+
+//enum Version: String, Codable {
+//    case the1281 = "12.8.1"
+//}
